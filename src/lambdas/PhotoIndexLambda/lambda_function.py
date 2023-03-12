4c050dc1a216d369c1758fa2ccf1e6e9c5c6a851
Alex Biehl <ab7289@nyu.edu>
2023-03-12 15:57:59 -0400

    added OpenSearch Domain URL to lambda, and added requirements file

--- src/lambdas/PhotoIndexLambda/lambda_function.py
+++ src/lambdas/PhotoIndexLambda/lambda_function.py
@@ -4,7 +4,13 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+## ENVIRONMENT ##################################################
+OS_DOMAIN: str = "https://vpc-csgy-9332a-hw2-photos-qndp6xmr6xhsptth25dxsx2lra.us-east-1.es.amazonaws.com"
+INDEX: str = "photos"
+ENDPOINT: str = "_doc"
 
 def lambda_handler(event: dict, context: dict) -> dict:
     logger.debug("Received s3 event: {}".format(event))
     logger.debug("Received context object: {}".format(context))
+
+    url: str = OS_DOMAIN + "/" + INDEX + "/" + ENDPOINT
