59a1540b332c0b783965dea992be91b13f029479
Alex Biehl <ab7289@nyu.edu>
2023-03-18 14:29:32 -0400

    added response to search event from lex, as well as updated Lex bot to correctly associated the Local with the bot alias

--- src/lambdas/PhotoSearchLambda/lambda_function.py
+++ src/lambdas/PhotoSearchLambda/lambda_function.py
@@ -7,6 +7,26 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+def disambiguate(query: str) -> list:
+    """
+    Utilizes our Lex bot to parse out what the user was asking
+
+    Parameters:
+    :queryString string: the text string that the user input
+    :returns: list of disambiguated search terms
+    """
+
+    client = boto3.client('lexv2-runtime')
+
+    response = client.recognize_text(
+        botId=os.getenv("LexBotId"),
+        botAliasId=os.getenv("LexBotAliasId"),
+        localeId='en_US',
+        sessionId='testsession',
+        text=query,
+    )
+    print(f"Lex response: {response}")
+
 
 def lambda_handler(event: dict, context: dict) -> None:
     logger.info("Received search event: {}".format(event))
@@ -14,6 +34,12 @@ def lambda_handler(event: dict, context: dict) -> None:
 
     print(f"received event: {event}")
     print(f"received context: {context}")
+
+    queryString: str = event["queryStringParameters"]["q"]
+
+    print(f"received queryString: {queryString}")
+
+    searchTerms: list = disambiguate(queryString)
 
     response: dict = {
         "isBase64Encoded": False,
