a60ea1749cab885c61e5975bfb8ae4ff1152da9d
Alex Biehl <ab7289@nyu.edu>
2023-03-18 13:47:50 -0400

    added example search event for lambda, updated PhotoSearchLambda permissions to create logs

--- src/lambdas/PhotoSearchLambda/lambda_function.py
+++ src/lambdas/PhotoSearchLambda/lambda_function.py
@@ -5,9 +5,22 @@ from botocore.exceptions import ClientError
 import logging
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event: dict, context: dict) -> None:
-    logger.debug("Received search event: {}".format(event))
-    logger.debug("Received context object: {}".format(context))
+    logger.info("Received search event: {}".format(event))
+    logger.info("Received context object: {}".format(context))
+
+    print(f"received event: {event}")
+    print(f"received context: {context}")
+
+    response: dict = {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": {},
+        "multiValueHeaders": {},
+        "body": "test body"
+    }
+
+    return response
