f7777194fa3a92c2dd519e3ec269915dacb6e756
Alex Biehl <ab7289@nyu.edu>
2023-03-25 14:19:54 -0400

    updated lambda function to return the appropriate response

--- src/lambdas/PhotoSearchLambda/lambda_function.py
+++ src/lambdas/PhotoSearchLambda/lambda_function.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import boto3
+import json
 from botocore.exceptions import ClientError
 from requests.auth import HTTPBasicAuth
 import logging
@@ -12,7 +13,8 @@ OS_DOMAIN: str = "https://" + os.getenv("OpenSearch_Domain")
 INDEX: str = "photos"
 SEARCH: str = "_search"
 
-def query_opensearch(searchTerms: list[str]) -> list[str]:
+
+def query_opensearch(searchTerms: list[str]) -> list[dict]:
     """
     Queries our opensearch index for images whose labels' array
     contains one of the search terms that user is querying for
@@ -22,9 +24,11 @@ def query_opensearch(searchTerms: list[str]) -> list[str]:
     :returns list: list of s3 object 
     """
     # strip out any trailing 's'
-    searchTerms = list(map(lambda x: x[:-1] if x.endswith("s") else x, searchTerms))
+    searchTerms = list(
+        map(lambda x: x[:-1] if x.endswith("s") else x, searchTerms))
     # repackage the search terms in the format necessary for the query
-    search_query: list = list(map(lambda x: {"term": {"labels": x}}, searchTerms))
+    search_query: list = list(
+        map(lambda x: {"term": {"labels": x}}, searchTerms))
     query: dict = {
         "query": {
             "bool": {
@@ -35,14 +39,45 @@ def query_opensearch(searchTerms: list[str]) -> list[str]:
 
     print(f"built search query: {query}")
 
-    headers: dict = { "Content-Type": "application/json" }
+    headers: dict = {"Content-Type": "application/json"}
     url: str = OS_DOMAIN + "/" + INDEX + "/" + SEARCH
 
-    auth = HTTPBasicAuth(os.getenv("OpenSearch_User"), os.getenv("OpenSearch_Password"))
+    auth = HTTPBasicAuth(os.getenv("OpenSearch_User"),
+                         os.getenv("OpenSearch_Password"))
     response = requests.get(url, headers=headers, json=query, auth=auth)
     response.raise_for_status()
-    print(f"openSearch response: {response.json()}")
-    return []
+    
+    return parse_os_response(response.json())
+
+
+def parse_os_response(response: dict) -> list[dict]:
+    """
+    Parses the Opensearch response
+    """
+    hits_obj: dict = response["hits"]
+    hit_count: int = hits_obj["total"]["value"]
+    if hit_count == 0:
+        logger.error("No hits received from opensearch")
+        return []
+    else:
+        logger.info("OpenSearch returned %s hits", hit_count)
+        hits_array: list = hits_obj["hits"]
+        return build_search_response(hits_array)
+
+
+def build_search_response(hits: list[dict]) -> list[dict]:
+    """
+    Taking in an array of hits from OpenSearch, builds the proper
+    response for our Search API
+    """
+    def mapper(hit: dict) -> dict:
+        source: dict = hit["_source"]
+        return {
+            "url": f"https://{source['bucket']}.s3.amazonaws.com/{source['objectKey']}",
+            "labels": source["labels"]
+        }
+
+    return list(map(mapper, hits))
 
 
 def disambiguate(query: str) -> list:
@@ -86,12 +121,14 @@ def lambda_handler(event: dict, context: dict) -> dict:
 
     s3_img_urls: list[str] = query_opensearch(searchTerms)
 
+    print(f"s3_img_urls: {s3_img_urls}")
+
     response: dict = {
         "isBase64Encoded": False,
-        "statusCode": 200,
-        "headers": {},
+        "statusCode": 200 if len(s3_img_urls) > 0 else 204,
+        "headers": {"Content-Type": "application/json"},
         "multiValueHeaders": {},
-        "body": ",".join(searchTerms)
+        "body": json.dumps(s3_img_urls)
     }
-
+    
     return response
