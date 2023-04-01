9efba6b4ad2141f297ef524783be2642b755ecc9
Alex Biehl <ab7289@nyu.edu>
2023-04-01 16:16:50 -0400

    updated index.js to properly parse the SearchResponse

--- s3/index.js
+++ s3/index.js
@@ -1,31 +1,31 @@
 
 window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition
 
-function voiceSearch(){
+function voiceSearch() {
     if ('SpeechRecognition' in window) {
         console.log("SpeechRecognition is Working");
     } else {
         console.log("SpeechRecognition is Not Working");
     }
-    
+
     var inputSearchQuery = document.getElementById("search_query");
     const recognition = new window.SpeechRecognition();
-   
 
-    micButton = document.getElementById("mic_search");  
-    
+
+    micButton = document.getElementById("mic_search");
+
     if (micButton.innerHTML == "mic") {
         recognition.start();
-    } else if (micButton.innerHTML == "mic_off"){
+    } else if (micButton.innerHTML == "mic_off") {
         recognition.stop();
     }
 
-    recognition.addEventListener("start", function() {
+    recognition.addEventListener("start", function () {
         micButton.innerHTML = "mic_off";
         console.log("Recording.....");
     });
 
-    recognition.addEventListener("end", function() {
+    recognition.addEventListener("end", function () {
         console.log("Stopping recording.");
         micButton.innerHTML = "mic";
     });
@@ -51,7 +51,7 @@ function textSearch() {
         console.log('Searching Photos....');
         searchPhotos(searchText);
     }
-    
+
 }
 
 function searchPhotos(searchText) {
@@ -61,35 +61,36 @@ function searchPhotos(searchText) {
     document.getElementById('photos_search_results').innerHTML = "<h4 style=\"text-align:center\">";
 
     var params = {
-        'q' : searchText
+        'q': searchText
     };
-    
-    apigClient.searchGet(params, {}, {})
-        .then(function(result) {
-            console.log("Result : ", result);
 
-            image_paths = result["data"]["body"]["imagePaths"];
-            console.log("image_paths : ", image_paths);
+    apigClient.searchGet(params, {}, {})
+        .then(function (result) {
+            console.log("Result : ", JSON.stringify(result));
+
+            image_paths = result["data"];
+            console.log("image_paths : ", JSON.stringify(image_paths));
 
             var photosDiv = document.getElementById("photos_search_results");
             photosDiv.innerHTML = "";
 
             var n;
             for (n = 0; n < image_paths.length; n++) {
-                images_list = image_paths[n].split('/');
-                imageName = images_list[images_list.length - 1];
+                // images_list = image_paths[n].split('/');
+                // imageName = images_list[images_list.length - 1];
+                const imageObj = image_paths[n];
 
-                photosDiv.innerHTML += '<figure><img src="' + image_paths[n] + '" style="width:25%"><figcaption>' + imageName + '</figcaption></figure>';
+                photosDiv.innerHTML += `<figure><img src="${imageObj.url}" style="width:25%"><figcaption></figcaption></figure>`;
             }
 
-        }).catch(function(result) {
+        }).catch(function (result) {
             var photosDiv = document.getElementById("photos_search_results");
             photosDiv.innerHTML = "Image not found";
             console.log(result);
         });
 }
 
-function getBase64(file){
+function getBase64(file) {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
         reader.onload = () => {
@@ -136,7 +137,7 @@ function uploadPhoto() {
     */
     var filePath = (document.getElementById('uploaded_file').value).split("\\");
     var fileName = filePath[filePath.length - 1];
-    
+
     if (!document.getElementById('custom_labels').innerText == "") {
         var customLabels = document.getElementById('custom_labels');
     }
@@ -147,11 +148,11 @@ function uploadPhoto() {
     var file = document.getElementById('uploaded_file').files[0];
     console.log('File : ', file);
     document.getElementById('uploaded_file').value = "";
-   
+
     if ((filePath == "") || (!['png', 'jpg', 'jpeg'].includes(fileName.split(".")[1]))) {
         alert("Please upload a valid .png/.jpg/.jpeg file!");
     } else {
-        
+
         var params = {};
         var additionalParams = {
             headers: {
@@ -160,17 +161,17 @@ function uploadPhoto() {
                 'x-amz-meta-customLabels': custom_labels.value
             }
         };
-        
+
         reader.onload = function (event) {
-             body = btoa(event.target.result);
+            body = btoa(event.target.result);
             console.log('Reader body : ', body);
             return apigClient.uploadItemPut(params, additionalParams)
-            .then(function(result) {
-                console.log(result);
-            })
-            .catch(function(error) {
-                console.log(error);
-            })
+                .then(function (result) {
+                    console.log(result);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
         }
         reader.readAsBinaryString(file);
     }
